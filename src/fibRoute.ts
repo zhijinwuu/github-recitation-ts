c3528c32687a64ba76e3bf2603486750a24ec604
zhijinwuu <zhijinw@andrew.cmu.edu>
2023-09-01 14:44:19 -0400

    change tupe in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,8 @@
 // Endpoint for querying the fibonacci numbers
-
+import {Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response)=> {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
